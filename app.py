8287620a95b4fab8d2a7ac566288ff908f8a9e45
Márcio Paulin <paulin.marcio@outlook.com>
2024-02-06 18:26:07 -0300

    refactor: add route '/'

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import os, re, datetime
+import db
+from models import Book
 app = Flask(__name__)
 
+if not os.path.isfile('books.db'):
+    db.connect()
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
+def isValid(email):
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     app.run()
