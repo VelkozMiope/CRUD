cced965cdce8005194c694f9e6a19f0c7e05270d
Márcio Paulin <paulin.marcio@outlook.com>
2024-02-06 18:26:53 -0300

    refactor: add /request post

--- app.py
+++ app.py
@@ -16,7 +16,37 @@ def isValid(email):
         return True
     else:
         return False
+    
+@app.route('/request', methods=['POST'])
+def postRequest():
+    req_data = request.get_json()
+    email = req_data['email']
+    if not isValid(email):
+        return jsonify({
+            'status':'422',
+            'res':'failure',
+            'error':'Invalid email format. Please enter a valid email address.'
+        })
+    title = req_data['title']
+    bks = [b.serialize() for b in db.view()]
+    for b in bks:
+        if b['title'] == title:
+            return jsonify({
+                'res': f'Error! Book with title {title} is already in library!',
+                'status': '404'
+            })
+        
+    bk = Book(db.getNewId(), True, title, datetime.datetime.now())
+    print('new book: ', bk.serialize())
+    db.insert(bk)
+    new_bks = [b.serialize() for b in db.view()]
+    print('books in lib: ', new_bks)
 
+    return jsonify({
+        'res': bk.serialize(),
+        'status': '200',
+        'msg': 'Success!'
+    })
 
 if __name__ == '__main__':
     app.run()
